8b28153f9ead26316cc1d4c1808b53b2667bbe2e
bgarma <aaron821124@gmail.com>
2018-08-02 20:45:00 +0800

    Work responsive

--- src/Components/About/components/Work.js
+++ src/Components/About/components/Work.js
@@ -1,6 +1,8 @@
-import React, { Component } from 'react';
+import React from 'react';
 import styled from 'styled-components';
+import MediaQuery from "react-responsive";
 import { timelineData } from '../Data';
+
 
 const Container = styled.div`
     display: flex;
@@ -45,45 +47,79 @@ const FlexBlock = styled.div`
     display: flex;
 `;
 
-const timeline = timelineData.map(function(e, i){
-    return (
-        <Container key={i}>
-            <LeftCol>
-                {e.date}
-                <Dot className="fa fa-circle" aria-hidden="true"/>
-            </LeftCol>
-            <RightCol>
-                <Title>{e.title}</Title>
-                <div style={{margin: '10px 0'}}>
-                    <FlexBlock>
-                        <div style={{marginRight: 10}}>
-                            <i className="fa fa-briefcase" aria-hidden="true"></i>
-                        </div>
-                        <div>{e.content}</div>
-                    </FlexBlock>
-                    <FlexBlock>
-                        <div style={{marginRight: 10}}>
-                            <i className="fa fa-wrench" aria-hidden="true"></i>
-                        </div>
-                        <div>{e.tools}</div>
-                    </FlexBlock>
-                </div>
-            </RightCol>
-        </Container>
-    );
-});
-
-
-
-class Work extends Component {
-    render() {
-        return (
-            <div>
-                {timeline}
+const timeline = timelineData.map((e) => (
+    <Container key={e.title}>
+        <LeftCol>
+            {e.date}
+            <Dot className="fa fa-circle" aria-hidden="true"/>
+        </LeftCol>
+        <RightCol>
+            <Title>{e.title}</Title>
+            <div style={{margin: '10px 0'}}>
+                <FlexBlock>
+                    <div style={{marginRight: 10}}>
+                        <i className="fa fa-briefcase" aria-hidden="true"/>
+                    </div>
+                    <div>{e.content}</div>
+                </FlexBlock>
+                <FlexBlock>
+                    <div style={{marginRight: 10}}>
+                        <i className="fa fa-wrench" aria-hidden="true"/>
+                    </div>
+                    <div>{e.tools}</div>
+                </FlexBlock>
             </div>
-        );
-    }
+        </RightCol>
+    </Container>
+));
 
-}
+const MobileContainer = styled.div`
+    width: 100%;
+    text-align: center;
+    max-width: 400px;
+    margin: auto;
+    
+    div {
+        line-height: 2em;
+    }
+`;
+
+const Line = styled.div`
+    border-right: #78ABB9 solid 3px;
+    height: 20px;
+    width: 0px;
+    margin: auto;
+`;
+
+const mobileTimeline = timelineData.map(e => (
+    <MobileContainer key={e.title}>
+        <Line/>
+        <div>
+            <i className="far fa-calendar-alt" />
+            &nbsp;&nbsp;
+            {e.date}
+        </div>
+        <div>
+            <i className="fa fa-briefcase" aria-hidden="true"/>
+            &nbsp;&nbsp;
+            {e.title}
+        </div>
+        <div>
+            <i className="fa fa-wrench" aria-hidden="true"/>
+            &nbsp;&nbsp;
+            {e.content}
+        </div>
+        <Line/>
+    </MobileContainer>
+))
+
+
+const Work = () => (
+    <MediaQuery minDeviceWidth={768}>
+        {(match) => <div>
+            {match? timeline : mobileTimeline}
+        </div>}
+    </MediaQuery>
+);
 
 export default Work;
